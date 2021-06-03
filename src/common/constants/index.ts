83065a9f3675967195e131a3354f72e533bad0ee
Josipa Braica <josipa.braica.drugi@gmail.com>
2021-06-03 23:01:39 +0200

    Add save, add and delete constants to constants file

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,3 +1,7 @@
 const NOTES_STORAGE_KEY = `${process.env.REACT_APP_PROJECT_NAME}/notes`;
 
-export { NOTES_STORAGE_KEY };
+const SAVE = "save";
+const ADD = "add";
+const DELETE = "delete";
+
+export { NOTES_STORAGE_KEY, SAVE, ADD, DELETE };
