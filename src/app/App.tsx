eb5b2cc559879e9abb695a372a0689c8dbf7df71
Josipa Braica <josipa.braica.drugi@gmail.com>
2021-06-04 17:21:55 +0200

    Wrap application with error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
+import { ErrorBoundary } from "react-error-boundary";
+
 import { NotesList } from "../features/notes";
+import { ErrorFallback } from "../common/components/error-fallback";
 
 function App() {
-  return <NotesList />;
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <NotesList />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
