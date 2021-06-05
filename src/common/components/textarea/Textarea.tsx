e332ca01737ccf10448f68e77e2ed92394e1b49c
Josipa Braica <josipa.braica.drugi@gmail.com>
2021-06-05 19:37:31 +0200

    Make reusable Textarea component

---
+++ src/common/components/textarea/Textarea.tsx
@@ -0,0 +1,13 @@
+import { FC, ChangeEvent } from "react";
+import { Textarea as StyledTextarea } from "./styled";
+
+interface Props {
+  value?: string;
+  onChange?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const Textarea: FC<Props> = ({ value, onChange }) => (
+  <StyledTextarea value={value} onChange={onChange} />
+);
+
+export default Textarea;
---
+++ src/common/components/textarea/styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  border: none;
+  resize: none;
+  font-family: Roboto Mono;
+  font-size: 14px;
+  line-height: 18px;
+`;
+
+export { Textarea };
