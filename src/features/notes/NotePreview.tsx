3ca065923968bc36ba6fb3a4b0b053abf564793f
Josipa Braica <josipa.braica.drugi@gmail.com>
2021-06-05 20:02:42 +0200

    Refactor NotePreview component

--- src/features/notes/NotePreview.tsx
+++ src/features/notes/NotePreview.tsx
@@ -1,34 +1,26 @@
 import { FC, memo } from "react";
 
-import { PreviewContent } from "./styled";
+import { Markdown } from "../../common/components/markdown";
 import { Card } from "../../common/components/card";
-import { useNotes } from "../../common/context/notesContext";
 import { Note } from "./types";
 
 interface Props {
-  // id: string;
   note: Note;
   handleClick: (id: string) => void;
 }
 
 const NotePreview: FC<Props> = ({ note, handleClick }) => {
   console.log("PREVIEW RERENDER", note.id);
-  // const { get } = useNotes();
-  // const note = get(id);
 
   const onClick = () => {
     handleClick(note.id);
   };
 
-  if (note) {
-    return (
-      <Card onClick={onClick}>
-        <PreviewContent>{note.content}</PreviewContent>
-      </Card>
-    );
-  }
-
-  return null;
+  return (
+    <Card onClick={onClick}>
+      <Markdown isPreview={true}>{note.content}</Markdown>
+    </Card>
+  );
 };
 
 export default memo(NotePreview);
